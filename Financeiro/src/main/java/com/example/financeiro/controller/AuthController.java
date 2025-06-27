bb00465f6b1de072529d7626b8c8c093b5c916ae
Kaique Magalhães Santos <kaiquemagalhaes27@gmail.com>
2025-06-26 21:54:25 -0300

    .

--- Financeiro/src/main/java/com/example/financeiro/controller/AuthController.java
+++ Financeiro/src/main/java/com/example/financeiro/controller/AuthController.java
@@ -2,24 +2,21 @@ package com.example.financeiro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
-
-
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/auth/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
 
-    @GetMapping("/auth/create")
+    @GetMapping("/sign")
     public String create() {
         return "create";
     }
